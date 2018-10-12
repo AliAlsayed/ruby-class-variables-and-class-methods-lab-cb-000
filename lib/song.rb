d58ce101c9fe28dd43031ddee91c88489aafd52d
Ali Alsayed <a_alsayed87@hotmail.com>
2018-10-12 09:52:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   def self.artist_count
     @@artists.each do |artist|
       if @@artist_count[artist]
-        @@genre_count[artist] += 1
+        @@artist_count[artist] += 1
       else
         @@artist_count[artist] = 1
       end
